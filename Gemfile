acd970d002fbbef4021813806955623604c24ad1
Imran Ahmad <imran.ahmad@navis.com>
2022-09-29 10:00:44 -0700

    added navbar and favicon

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+#added semantic ui gem - imran
+gem 'semantic-ui-sass'
+
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "bootstrap"
+import "../stylesheets/application"
+
+
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+$(document).on('turbolinks:load', function functionName() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  });
+})
