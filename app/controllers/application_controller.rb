7f675029a1e3416458e2ec68da3ac85990763538
Imran Ahmad <imran.ahmad@navis.com>
2022-09-29 21:45:59 -0700

    Created login and destroy routes and Ui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  # following will make the current_user method availabel in the views as well
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # following syntax (with ||) will retyrn current_user if it is already there
+    # and not hit the database each time the current_user method is called
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # following syntax with !! converts to boolean, meaning if @current_user has
+    # a value below will return true
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform this action"
+      redirect_to login_path, status: :see_other
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,39 @@
 class SessionsController < ApplicationController
   def new
+    #debugger
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    # if user && user.authenticate(params[:session][:password])
+    #   # good scenario
+    #   session[:user_id] = user.id
+    #   flash[:success] = "You have successfully logged in"
+    #   redirect_to root_path
+    # else
+    #   flash.now[:error]= "There was something wrong with your login information"
+    #   render 'new', status: :see_other
+    # end
+
+
+    respond_to do |format|
+      if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:success] = "Logged in successfully"
+        format.html { redirect_to root_path }
+      else
+        flash.now[:error] = "There was something wrong with your login!"
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def destroy
+    #debugger
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path, status: :see_other
+  end
+
 end
